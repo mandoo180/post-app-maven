aab6cc045d6ef74e6380c86d1e9347cf2b4ef545
tom <mandoo180@gmail.com>
2020-07-05 14:39:07 +0900

    first commit

---
+++ src/main/java/com/fayewana/App.java
@@ -0,0 +1,14 @@
+package com.fayewana;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	System.out.println("What the hell is this?");
+    }
+}
